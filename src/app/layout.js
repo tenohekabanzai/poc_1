86b93d9d36c7935bc45ad3906f076189e11bcb61
tenohekabanzai <10ohekabanzai@gmail.com>
2025-03-27 11:45:00 +0530

    2nd

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,19 @@
-"use client"; // Enables client-side rendering for path detection
-
-import { usePathname } from "next/navigation";
+// Enables client-side rendering for path detection
+// "use client"
+// import { usePathname } from "next/navigation";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import AdminComp from "@/components/adminComp";
-import CustomerComp from "@/components/CustomerComp";
+import PathProvider from "./pathProvider";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
-  const pathname = usePathname(); // Get the current route
-
-  // Check if the user is in the admin section
-  const isAdmin = pathname.startsWith("/admin");
 
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {isAdmin ? (
-          <AdminComp children={children}/>
-        ) : (
-          <CustomerComp children={children}/>
-        )}
+      <PathProvider>{children}</PathProvider>
       </body>
     </html>
   );
